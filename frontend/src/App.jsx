85569388d7f45009e8aeea058e8c61df1ab57603
eyubbalcan <126914485+eyubbalcan@users.noreply.github.com>
2023-10-25 17:28:54 +0300

    1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './companents/Footer'
+import Header from './companents/Header'
+import Policy from './companents/Policy'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+      <Policy />
+      <Footer />
+    </>
+  )
+}
+
+export default App
